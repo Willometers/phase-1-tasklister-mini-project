ae838e218fbc144f54c286f07b7d408257700730
BillyMiles <williammiles201@gmail.com>
2021-05-12 16:16:54 -0400

    Finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const text = document.getElementById("new-task-description");
+  const list = document.getElementById("list");
+  const tasks = document.getElementById("tasks");
+  const li = document.createElement('li')
+  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    tasks.appendChild(li);
+    li.innerText = text.value
+  })
 });
